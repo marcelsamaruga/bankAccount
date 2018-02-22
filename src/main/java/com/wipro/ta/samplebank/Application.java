26f0f0c90d4c75d48804e0de51efb4b830a4643b
Marcel Samaruga da Costa <marcelsamaruga@gmail.com>
2018-02-21 21:51:31 -0300

    Commit

--- src/main/java/com/wipro/ta/samplebank/Application.java
+++ src/main/java/com/wipro/ta/samplebank/Application.java
@@ -13,6 +13,7 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
